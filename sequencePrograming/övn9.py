4077f7ac39f3921ab284ffc3fcd844d6270fa4cc
mig-25 <kd3180@hotmail.com>
2022-12-14 14:15:47 +0100

    update övn 9

--- sequencePrograming/övn9.py
+++ sequencePrograming/övn9.py
@@ -1,25 +1,19 @@
 """ Ange radie för en cirkel,
 beräkna diameter, omkrets och area
 Enbart en funktion för användas, använd en konstant för Pi
-Svaret ska vara med decimaler """
+Svaret ska vara med två decimaler """
 
 import constant
 
-radie = 6
-
-
-def cirkel():
+def cirkel(radie):
     diameter = 2*radie
     omkrets = diameter*constant.PI
     area = radie*radie*constant.PI
     return diameter, omkrets, area
 
 
-''' d = float(input("Mata in diametern: "))
-o = float(input("Mata in omkretsen: "))
-a = float(input("Mata in arean: ")) '''
-
-diameter, omkrets, area = cirkel()
+r = float(input("Mata in radien: "))
+diameter, omkrets, area = cirkel(r)
 
 print(
     f"Diametern är: {diameter:.2f}, omkretsen är: {omkrets:.2f} och arean är: {area:.2f}")

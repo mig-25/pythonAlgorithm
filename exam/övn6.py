b8304d15041c65eec7419ec778f19cab0fec8fdd
mig-25 <kd3180@hotmail.com>
2023-01-10 17:44:49 +0100

    exam question

--- exam/övn6.py
+++ exam/övn6.py
@@ -1,3 +1,39 @@
+""" Skapa två dictionaries som heter education och courses. 
+Via en input ska ni spara skolansnamn och utbildningsnamn och namnet på studenten. 
+
+Reptera SÅ LÄNGE ni vill kunna lägga till kursID och kursNamn, antal poäng för kursen och betyg
+via en input, gör sedan följande:
+1. lägg till dessa input läggas till i dictionary för course (alltså för en enskild kurs)
+
+2. När ni lagt till ovan värden i dictionary för course, lägg då samtliga enskilda
+kurser i dictionary för courses (alltså för en dictionariy för alla kurser)
+
+3. Om ni inte längre vill repetera och lägga till fler kurs i kurser avsluta loopen
+
+4a. lägg till courses i dictionary för education
+4b. lägg till skolansnamn i dictionary för education
+4c. lägg till utbildningensnamn i dictionary för education
+4d. lägg till studentensnamn i dictionary för education
+
+5. Skapa en textfil som heter education.txt
+
+6. Om det finns någon kurs som har betyget IG, så ska rubriken för filen vara
+Utbildningsbevis, annars om det är minst G så ska det stå Examensbevis som rubrik
+
+7. Textfilen ska innehålla följande när hela programmet har kört klart, dvs ni måste
+i resten av koden koda för följande:
+
+Utfärdat: Utbildningsbevis	för studenten: kalle anka 
+Skola: xenter 	Utbildning: itsak22 	Total antal poäng: 85 
+kurs id: itsaksql
+namn: sql, 35 poäng, betyg: IG
+kurs id: itsakpyt
+namn: python, 30 poäng, betyg: G
+kurs id: itsakagl
+namn: agile, 20 poäng, betyg: VG
+
+"""
+
 def educations():
     # initialize an empty dictionary to store courses
     courses = {}
